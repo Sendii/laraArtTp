72d7d8c93a2470f0e05fa38b06a171f9be877438
Sendaay <rahmamns1@gmail.com>
2018-09-27 20:42:02 +0700

    scnd

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,9 @@
 |
 */
 
+Auth::routes();
 Route::get('/', function () {
 	return view('welcome');
 });
-
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('art', 'ArtikelController');
